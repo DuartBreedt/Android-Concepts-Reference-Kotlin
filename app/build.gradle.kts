fd5634e360c243a282888faa06958b9aa6933f14
Duart Breedt <duartbreedt@gmail.com>
2023-02-14 07:05:28 +0200

    Added kotlin support dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
