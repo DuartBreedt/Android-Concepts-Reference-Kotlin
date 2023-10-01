8380e7d87675fd2410a3af0cd17d4942d61df364
Duart Breedt <duartbreedt@gmail.com>
2023-10-01 09:41:16 +0200

    Added basic Activity lifecycles

--- app/src/main/java/za/co/duartbreedt/androidconceptsreference/MainActivity.kt
+++ app/src/main/java/za/co/duartbreedt/androidconceptsreference/MainActivity.kt
@@ -1,6 +1,7 @@
 package za.co.duartbreedt.androidconceptsreference
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import za.co.duartbreedt.androidconceptsreference.databinding.ActivityMainBinding
 
@@ -11,8 +12,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Log.i("BOOP", "onCreate")
+
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("BOOP", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("BOOP", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("BOOP", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("BOOP", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("BOOP", "onDestroy")
     }
 }
