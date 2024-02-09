e287870099ba5f3ce7faffd5a9b9d839d2387428
Niksha Simney <EC858501T1@standardbank.onmicrosoft.com>
2024-02-09 11:10:41 +0200

    Initial commit

--- app/src/main/java/za/co/duartbreedt/androidconceptsreference/MainActivity.kt
+++ app/src/main/java/za/co/duartbreedt/androidconceptsreference/MainActivity.kt
@@ -6,13 +6,9 @@ import za.co.duartbreedt.androidconceptsreference.databinding.ActivityMainBindin
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
